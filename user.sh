3aa4ed22fc94939f5acf047fe84ac1ec7a6d00d5
rendiap100 <156988494+rendiap100@users.noreply.github.com>
2024-09-21 15:06:15 +0700

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Mendapatkan tanggal dari server
+# tanggal dari server
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=$(date +"%Y-%m-%d" -d "$dateFromServer")
 
-# Membersihkan layar
 clear
 
-# Menghitung jumlah klien
+# jumlah klien
 NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
 
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
@@ -17,9 +16,8 @@ if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 fi
 
 clear
-echo "TAMPILKAN INFORMASI AKUN XRAY VMESS WS"
+echo "TAMPILKAN INFORMASI AKUN XRAY VMESS "
 echo "Silakan pilih klien yang ingin ditampilkan informasinya"
-echo "Tekan CTRL+C untuk kembali"
 echo "==============================="
 grep -E "^#! " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
 
@@ -30,8 +28,6 @@ read -rp "Pilih klien [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 domain=$(cat /etc/xray/domain)
 export user=$(grep -E "^#! " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 export exp=$(grep -E "^#! " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-
-# Mendapatkan UUID berdasarkan email
 export uuid=$(grep -E "\"email\": \"${user}\"" "/etc/xray/config.json" | cut -d '"' -f 4 | head -n 1)
 
 # Menampilkan informasi akun
