1ff0b5a91ead6de0a4b616a158cf4b50ee372f08
rendiap100 <156988494+rendiap100@users.noreply.github.com>
2024-10-08 15:57:59 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -389,24 +389,32 @@ rm -rf /etc/vmess/.vmess.db
     echo "& plughin Account" >>/etc/ssh/.ssh.db
     echo "echo -e 'Vps Config User Account'" >> /etc/user-create/user.log
     }
+    
 #Instal Xray
 function install_xray() {
-clear
-    print_install "Core Xray 1.8.21 Latest Version"
-    domainSock_dir="/run/xray";! [ -d $domainSock_dir ] && mkdir  $domainSock_dir
+    clear
+    print_install "Core Xray 1.8.23 Latest Version"
+    
+    # Membuat direktori domain socket jika belum ada
+    domainSock_dir="/run/xray"
+    ! [ -d $domainSock_dir ] && mkdir $domainSock_dir
     chown www-data.www-data $domainSock_dir
     
-    # / / Ambil Xray Core Version Terbaru
-latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.21
+    # Mengambil Xray Core Version Terbaru
+    latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+    
+    # Menginstall Xray dengan versi 1.8.23
+    bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.23
  
-    # // Ambil Config Server
+    # Mengambil Config Server
     wget -O /etc/xray/config.json "${REPO}config/config.json" >/dev/null 2>&1
     wget -O /etc/systemd/system/runn.service "${REPO}files/runn.service" >/dev/null 2>&1
-    #chmod +x /usr/local/bin/xray
+    
     domain=$(cat /etc/xray/domain)
     IPVS=$(cat /etc/xray/ipvps)
-    print_success "Core Xray 1.8.21 Latest Version"
+    
+    print_success "Core Xray 1.8.23 Latest Version"
+}
     
     # Settings UP Nginix Server
     clear
